607e8a0c42a8fa007447038497cb8e5200927584
Yoonjin <thisisthewa2@gmail.com>
2023-11-07 01:06:47 +0900

    🚧 feat: Button 컴포넌트 구현

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,8 @@
-const IMAGES = {};
+import arrowImg from '../assets/arrow-right.svg';
+import brownArrowImg from '../assets/arrow-right-brown.svg';
+import facebookImg from '../assets/Facebook.svg';
+import kakaoImg from '../assets/Kakao.svg';
+import linkImg from '../assets/Link.svg';
+const IMAGES = { arrowImg, brownArrowImg, facebookImg, linkImg, kakaoImg };
 
 export default IMAGES;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
+import Button from '../components/common/Button/Button';
+import FloatingButton from '../components/common/Button/FloatingButton';
+import FacebookButton from '../components/common/Button/FacebookButton';
+import LinkButton from '../components/common/Button/LinkButton';
+import KakaoButton from '../components/common/Button/KakaoButton';
 const HomePage = () => {
   return (
     <>
-      <p>sdfdsf</p>
+      <Button type="question" text="질문 받기" />
+      <div>hi</div>
+      <Button type="answer" text="답변하러 가기" />
+      <FloatingButton />
+      <FacebookButton />
+      <LinkButton />
+      <KakaoButton />
     </>
   );
 };
