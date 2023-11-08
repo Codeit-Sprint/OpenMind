40ad660a246aa53f6a0123c93ece6775f78041c7
qooktree1 <qooktree@gmail.com>
2023-11-08 14:12:00 +0900

    🎨 style: Button css theme -> props.theme으로 수정

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,7 +1,5 @@
-/* eslint-disable react/prop-types */
 import styled from 'styled-components';
 import IMAGES from '../../../assets';
-import theme from '../../../styles/theme';
 import { device } from '../../../styles/mediaQuery';
 import * as S from '../../../styles/typography';
 
@@ -12,14 +10,16 @@ const Icon = styled.img`
 
 const StyledButton = styled.button`
   background-color: ${(props) =>
-    props.type === 'answer' ? theme['brown-10'] : theme['brown-40']};
+    props.type === 'answer'
+      ? props.theme['brown-10']
+      : props.theme['brown-40']};
   display: flex;
   width: ${(props) => (props.type === 'answer' ? '192px' : '160px')};
   height: 46px;
-  border: 1px solid ${theme['brown-40']};
+  border: 1px solid ${(props) => props.theme['brown-40']};
   border-width: ${(props) => (props.type === 'answer' ? '1px' : '0px')};
   color: ${(props) =>
-    props.type === 'answer' ? theme['brown-40'] : '#FFFFFF'};
+    props.type === 'answer' ? props.theme['brown-40'] : '#FFFFFF'};
   gap: 10px;
   padding: 12px 24px 12px 24px;
   border-radius: 8px;
@@ -31,24 +31,30 @@ const StyledButton = styled.button`
   & ${Icon} {
   }
 
-  &: hover {
+  &:hover {
     border: 2px solid;
     border-color: ${(props) =>
-      props.type === 'answer' ? theme['brown-40'] : theme['brown-50']};
+      props.type === 'answer'
+        ? props.theme['brown-40']
+        : props.theme['brown-50']};
   }
 
   &:active {
     background-color: ${(props) =>
-      props.type === 'answer' ? theme['brown-20'] : theme['brown-50']};
+      props.type === 'answer'
+        ? props.theme['brown-20']
+        : props.theme['brown-50']};
     border-width: ${(props) => (props.type === 'answer' ? '2px' : '0px')};
   }
 
   &:disabled {
     background-color: ${(props) =>
-      props.type === 'answer' ? theme['brown-10'] : theme['brown-30']};
+      props.type === 'answer'
+        ? props.theme['brown-10']
+        : props.theme['brown-30']};
   }
   border-width: ${(props) => (props.type === 'answer' ? '2px' : '0px')};
-  border-color: theme[ 'brown-30'];
+  border-color: ${(props) => props.theme['brown-30']};
   ${S.BodyRegular3};
 
   @media ${device.mobile} {
