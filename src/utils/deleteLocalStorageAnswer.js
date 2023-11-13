5b48b0569e85f8026003d2c3fc3cfdaa7f1c809a
qooktree1 <qooktree@gmail.com>
2023-11-13 17:07:43 +0900

    📌 fix: deleteLocalStorageAnswer 함수 인자 못받아오는 오류 해결

--- src/utils/deleteLocalStorageAnswer.js
+++ src/utils/deleteLocalStorageAnswer.js
@@ -1,5 +1,5 @@
 // questionId에 해당하는 객체들 제거
-export const deleteLocalStorageAnswer = (questionId) => {
+export const deleteLocalStorageAnswer = ({ questionId }) => {
   const answerArr = JSON.parse(localStorage.getItem('answerArray'));
   const newArr = answerArr.map((answer) => answer.questionId !== questionId);
   localStorage.setItem('answerArray', JSON.stringify(newArr));
