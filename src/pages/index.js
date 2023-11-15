44c97278bc9b3f04a76a39e420583be460002e43
qooktree1 <qooktree@gmail.com>
2023-11-15 15:37:32 +0900

    📌 fix: NotFoundPage index.js에 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,5 @@ import HomePage from './HomePage/HomePage';
 import QuestionListPage from './QuestionListPage/QuestionListPage';
 import FeedPage from './FeedPage/FeedPage';
 import AnswerPage from './AnswerPage/AnswerPage';
-export { HomePage, QuestionListPage, FeedPage, AnswerPage };
+import NotFoundPage from './NotFoundPage/NotFoundPage';
+export { HomePage, QuestionListPage, FeedPage, AnswerPage, NotFoundPage };
